350b3824c11df296c9abde1c576413f6b565b1fa
Thallis <thallis.faria@newrizon.global>
2025-08-23 15:05:25 -0300

    fix: change EKS node group instance type from t3.nano to t3.micro for better compatibility

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_eks_node_group" "fiap_x_eks_node_group" {
   cluster_name    = var.aws_cluster_name
   node_group_name = "${var.aws_cluster_name}-nodegroup"
   node_role_arn   = "arn:aws:iam::${var.aws_account_id}:role/LabNodeRole"
-  instance_types  = ["t3.nano"]
+  instance_types  = ["t3.micro"]
   disk_size       = 20
   ami_type        = "AL2023_x86_64_STANDARD"
   capacity_type   = "ON_DEMAND"
